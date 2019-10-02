06b482f392c124fe53503b49a73a5d2b0ed2c8ec
hjin36 <37239988+hjin36@users.noreply.github.com>
2019-10-02 16:17:28 -0400

    custom.js

--- sklearn.js
+++ sklearn.js
@@ -1,18 +1,20 @@
-{
-  "name": "scikit-learn",
-  "sub-menu": [
-    {
-      "name": "Processing",
-      "sub-menu": [
-        {
-          "name": "Train Test Split",
-          "snippet": ["from sklearn.model_selection import train_test_split",
+require(["nbextensions/snippets_menu/main"], function(snippets_menu) {
+  console.log('Loading `snippets_menu` customizations from `custom.js`');
+  var sklearn = {
+    "name": "Sklearn",
+    "sub-menu": [
+      {
+        "name": "Processing",
+        "sub-menu": [
+          {
+            "name": "Train Test Split",
+            "snippet": ["from sklearn.model_selection import train_test_split",
                       "X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)"
                      ]
         },
-        {
-          "name": "Standard Scale",
-          "snippet": ["from sklearn.preprocessing import StandardScaler",
+          {
+            "name": "Standard Scale",
+            "snippet": ["from sklearn.preprocessing import StandardScaler",
                       "sc = StandardScaler()",
                       "sc.fit(X)",
                       "X = sc.transform(X)"
@@ -20,12 +22,12 @@
         }
       ]
     },
-    {
-      "name": "Feature Engineering",
-      "sub-menu": [
-        {
-          "name": "L1 Feature Selection",
-          "snippet": ["from sklearn.linear_model import LogisticRegression",
+      {
+        "name": "Feature Engineering",
+        "sub-menu": [
+          {
+            "name": "L1 Feature Selection",
+            "snippet": ["from sklearn.linear_model import LogisticRegression",
                       "lr = LogisticRegression(penalty='l1', C=1.0)",
                       "lr.fit(X_train, y_train)",
                       "print('Training accuracy:', lr.score(X_train, y_train))",
@@ -34,44 +36,44 @@
         }
       ]
     },
-    {
-      "name": "Classification",
-      "sub-menu": [
-        {
-          "name": "Decision Tree",
-          "snippet": ["dt = DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=5",
+      {
+        "name": "Classification",
+        "sub-menu": [
+          {
+            "name": "Decision Tree",
+            "snippet": ["dt = DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=5",
                       "min_impurity_decrease=0.0, min_samples_leaf=6, min_samples_split=2,random_state=42)"
                      ]
         },
-        {
-          "name": "Logistic Regression",
-          "snippet": ["logr = LogisticRegression(penalty='l1', C=100,random_state=42)"]
+          {
+            "name": "Logistic Regression",
+            "snippet": ["logr = LogisticRegression(penalty='l1', C=100,random_state=42)"]
         },
-        {
-          "name": "K-Nearest Neighbors",
-          "snippet": ["kn = KNeighborsClassifier(n_neighbors=9,p=2",
+          {
+            "name": "K-Nearest Neighbors",
+            "snippet": ["kn = KNeighborsClassifier(n_neighbors=9,p=2",
                       "metric='minkowski',weights='uniform')"
                      ]
         }
       ]
     },
-    {
-      "name": "Regression",
-      "sub-menu": [
-        {
-          "name": "Linear Regression",
-          "snippet": ["from sklearn.linear_model import LinearRegression",
+      {
+        "name": "Regression",
+        "sub-menu": [
+          {
+            "name": "Linear Regression",
+            "snippet": ["from sklearn.linear_model import LinearRegression",
                       "lr = LinearRegression(fit_intercept=True, normalize=False, copy_X=True, n_jobs=None)"
                      ]
         }
       ]
     },
-    {
-      "name": "Unsupervised Learning",
-      "sub-menu": [
-        {
-          "name": "K-means Clustering",
-          "snippet": ["from sklearn.cluster import KMeans",
+      {
+        "name": "Unsupervised Learning",
+        "sub-menu": [
+          {
+            "name": "K-means Clustering",
+            "snippet": ["from sklearn.cluster import KMeans",
                       "from scipy.spatial.distance import cdist",
                       "kmeans = KMeans(n_clusters=4)\n#elbow method",
                       "distortions = []",
@@ -84,22 +86,26 @@
         }
       ]
     },
-    {
-      "name": "Evaluation",
-      "sub-menu": [
-        {
-          "name": "Confusion Matrix",
-          "snippet": ["from sklearn.metrics import confusion_matrix",
+      {
+        "name": "Evaluation",
+        "sub-menu": [
+          {
+            "name": "Confusion Matrix",
+            "snippet": ["from sklearn.metrics import confusion_matrix",
                       "cnf_matrix = confusion_matrix(y_test, y_pred)"
                      ]
         },
-        {
-          "name": "Classification Report",
-          "snippet": ["from sklearn.metrics import classification_report",
+          {
+            "name": "Classification Report",
+            "snippet": ["from sklearn.metrics import classification_report",
                       "print(classification_report(y_test, y_pred))"
                      ]
         }
       ]
     }
     ]
-}
+  };
+  snippets_menu.options['menus'].push(snippets_menu.default_menus[0]); // Start with the remaining "Snippets" menu
+  snippets_menu.options['menus'].push(sklearn); 
+  console.log('Loaded `snippets_menu` customizations from `custom.js`');
+});
