30f908af8830830ffd39b80e4b27c3f06a6f784e
hjin36 <37239988+hjin36@users.noreply.github.com>
2019-10-02 14:12:54 -0400

    a few edits

--- sklearn.js
+++ sklearn.js
@@ -67,6 +67,24 @@
       ]
     },
     {
+      "name": "Unsupervised Learning",
+      "sub-menu": [
+        {
+          "name": "K-means Clustering",
+          "snippet": ["from sklearn.cluster import KMeans",
+                      "from scipy.spatial.distance import cdist",
+                      "kmeans = KMeans(n_clusters=4)\n#elbow method",
+                      "distortions = []",
+                      "K = range(1,10)",
+                      "for k in K:",
+                      "kmeanModel = KMeans(n_clusters=k).fit(X)",
+                      "kmeanModel.fit(X)",
+                      "distortions.append(sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])"
+                     ]
+        }
+      ]
+    },
+    {
       "name": "Evaluation",
       "sub-menu": [
         {
